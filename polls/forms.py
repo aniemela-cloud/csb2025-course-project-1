3ad160cfc0211daac9f72b88f943a000f84b2a76
aniemela-cloud <aniemela@gmail.com>
2026-07-16 18:43:54 +0300

    Django "poll" app from tutorial, with a stub user form for creating a new poll question with 2-3 options

---
+++ polls/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class QuestionForm(forms.Form):
+  question_text = forms.CharField(label="Your question", max_length=200)
+  choice_1_text = forms.CharField(label="The first choice", max_length=200)
+  choice_2_text = forms.CharField(label="The second choice", max_length=200)
+  choice_3_text = forms.CharField(label="The third choice", max_length=200, required=False)
+  
